864f5965dacd0add53b854233ec9b77768aaa666
İbrahim Ethem Şen <ibrahim-ethem-50@hotmail.com>
2023-02-16 14:57:36 +0300

    AvatarView

--- app/src/main/java/com/ibrahimethemsen/customview101/AvatarView.kt
+++ app/src/main/java/com/ibrahimethemsen/customview101/AvatarView.kt
@@ -1,4 +1,43 @@
 package com.ibrahimethemsen.customview101
 
-class AvatarView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
+import androidx.annotation.DrawableRes
+import com.ibrahimethemsen.customview101.databinding.AvatarViewBinding
+
+class AvatarView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null
+) : FrameLayout(context, attributeSet) {
+
+    private val binding = AvatarViewBinding.inflate(LayoutInflater.from(context), this, false)
+
+    init {
+        addView(binding.root)
+    }
+
+    fun setAvatar(avatar: Avatar) {
+        if (avatar.avatarImage != null) {
+            binding.avatarImage.apply {
+                visibility = View.VISIBLE
+                setImageResource(avatar.avatarImage)
+            }
+        } else {
+            binding.apply {
+                avatarImage.visibility = View.GONE
+                val name = avatar.avatarName?.first()?.uppercase()
+                val surname = avatar.avatarSurname?.first()?.uppercase()
+                val title = context.getString(R.string.avatar_name_surname, name, surname)
+                avatarText.text = title
+            }
+        }
+    }
 }
+data class Avatar(
+    @DrawableRes val avatarImage: Int? = null,
+    val avatarName: String? = null,
+    val avatarSurname: String? = null
+)
--- app/src/main/java/com/ibrahimethemsen/customview101/MainActivity.kt
+++ app/src/main/java/com/ibrahimethemsen/customview101/MainActivity.kt
@@ -2,10 +2,17 @@ package com.ibrahimethemsen.customview101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ibrahimethemsen.customview101.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.avatarView.setAvatar(Avatar(avatarImage = R.drawable.ic_launcher_foreground))
+
     }
 }
